087df7da067ba20e368c1ee1078e6e051686a72a
marynag <mgrabchak1109@gmail.com>
2022-09-16 22:17:44 +0300

    split to proper files

---
+++ tic-toc-toe/src/Game.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import './index.css';
+import Timer from './timer.js'
+import { getGameStatus } from './timer.util';
+import Board from './Board';
+import { calculateWinner } from './game.utils';
+
+
+export class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+  
+
+  shouldComponentUpdate(nextState) {
+    return this.state.history !== nextState.history;
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+  render() {   
+
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    const {isGameEnded, displayStatus} = getGameStatus(history, winner, this.state.xIsNext);
+
+    return ( 
+      <>
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{displayStatus}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+      <Timer isGameFinished={isGameEnded}/>
+      </>
+    );
+  }
+}
+
+
+
+
+
