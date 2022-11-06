5739b6a754f6e19325167fc07b02d94358b67928
marynag <mgrabchak1109@gmail.com>
2022-11-06 14:28:20 +0200

    fix timer useefect

--- tic-toc-toe-func/src/timer/timer.js
+++ tic-toc-toe-func/src/timer/timer.js
@@ -1,22 +1,28 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import convertTime from './timer.util'
+import {useHistory} from "../game/useHistory";
 
 export const Timer = (props) =>{
   const [time, setTime] = useState(0);
 
+  const intervalId=useRef()
+
   const {isGameFinished} = props;
 
-  useEffect(() =>{
-    let interval = null;
-    if(!isGameFinished){
-      interval = setInterval(() => {
-        setTime(time => time + 1);
+  //analogue of the constructor or componentMount
+  useEffect (() =>{
+      intervalId.current =  setInterval(() => {
+          setTime(time => time + 1);
       }, 1000);
-    }else{
-      clearInterval(interval)
-    }
-    return () => clearInterval(interval);
-  }, [time]);
+      return () => clearInterval(intervalId.current)
+  },[])
+
+    useEffect(()=>{
+        if(!isGameFinished){
+            return
+        }
+        clearInterval(intervalId.current)
+    },[isGameFinished])
 
   const convertedTime=convertTime(time)
 
