6ec0b69f8c17445bda9737f1af49fdcab41b12c4
marynag <mgrabchak1109@gmail.com>
2023-02-13 16:26:12 +0200

    remove useEffect in Breeds component and fix variables naming

--- cats-gallery/src/components/breeds/breeds.jsx
+++ cats-gallery/src/components/breeds/breeds.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import vector5 from '../../img/ba.png'
-import vector6 from '../../img/ab.png'
+import React, { useState } from 'react';
+import ImgSort1 from '../../img/ba.png'
+import ImgSort2 from '../../img/ab.png'
 import { LIMITS, GRID_PHOTO_CLASSES} from './breed.constants'
 import styles from './breeds.module.scss'
 import { SearchImg }  from '../searchImg';
@@ -9,29 +9,20 @@ import { useSelector } from 'react-redux';
 
 
 export const Breeds = () => {
-    const [breedList, setBereedList] = useState()
     const [selectedBreedId, setSelectedBreedId] = useState()
-    const [breedNameId, setBreedNameId] = useState();
-    const [breedName, setBreedName] = useState('Bengal');
+    const [breedName, setBreedName] = useState('');
     const [imgAmount, setImgAmount] = useState(GRID_PHOTO_CLASSES.slice(0,5))
     const [inputId, setInputId] = useState();
 
     const breedInfo = useSelector(state => state.breeds)
 
-    useEffect(()=>{
-        if(breedInfo){
-            const breed = breedInfo.reduce((acc, item) =>{
-                acc[item.name]=item.id
-                return acc
-            }, {})
+    const breed = breedInfo.reduce((acc, item) =>{
+        acc[item.name]=item.id
+        return acc
+    }, {})
 
-            const breedName = Object.keys(breed)
-
-            setBereedList(breedName)
-            setBreedNameId(breed)
-        }
-
-    },[breedInfo])
+    const breeds = Object.keys(breed)
+    const breedNameId = breed
 
     const handleBreedChange = (event) => {
         const selectedBreed=event.target.value
@@ -54,13 +45,13 @@ export const Breeds = () => {
                     <p className={styles.next}>&lt;</p>
                     <p className={styles.vote}>BREEDS</p>
                     <select className={`${styles.vote} ${styles.breedSelect}`} onChange={handleBreedChange}>
-                        {breedList ? breedList.map(current => <option key={current}>{current}</option> ): null}
+                        {breeds ? breeds.map(current => <option key={current}>{current}</option> ): null}
                     </select>
                     <select className={`${styles.vote} ${styles.breedsLimit}`} onChange={handleLimitChange} >
                         {LIMITS.map(current => <option key={current} value={current}>Limit: {current}</option> )}
                     </select>
-                    <div className={styles.sortImg}><img className={styles.sort} src={vector5} alt="sort" /></div>
-                    <div className={styles.sortImg}><img className={`${styles.sort} ${styles.sort1}`} src={vector6} alt="sort" /></div>
+                    <div className={styles.sortImg}><img className={styles.sort} src={ImgSort1} alt="sort" /></div>
+                    <div className={styles.sortImg}><img className={`${styles.sort} ${styles.sort1}`} src={ImgSort2} alt="sort" /></div>
                 </div>
             </div>
             <div className={styles.catImgBreedsWrapper}>
--- cats-gallery/src/components/searchImg/searchImg.jsx
+++ cats-gallery/src/components/searchImg/searchImg.jsx
@@ -1,13 +1,13 @@
-import vector from '../../img/Vector.png'
+import imgSearch from '../../img/Vector.png'
 import styles from './searchImg.module.scss'
 import React, { useState } from 'react';
 import {REACTIONS} from "./searchImg.utils";
 
 export const SearchImg = (props) => {
-    const [userInput, setuserInput] = useState('');
+    const [userInput, setUserInput] = useState('');
 
     const handleChange = (event) => {
-        setuserInput(event.target.value)
+        setUserInput(event.target.value)
     }
 
     const handleClick = () =>{
@@ -18,7 +18,7 @@ export const SearchImg = (props) => {
         <div className={styles.searchingLine}>
                 <div className={styles.search}>
                     <input type="text" value={userInput} column placeholder="Search image by id" className={styles.searchInput} onChange={handleChange}/>
-                    <div className={styles.searchItem} onClick={handleClick}><img src={vector} alt="girl and pet" className={styles.search_img}/></div>
+                    <div className={styles.searchItem} onClick={handleClick}><img src={imgSearch} alt="search" className={styles.search_img}/></div>
                 </div>
             { REACTIONS.map(current => (<div key={current.name} className={styles.reaction}>
                 <img src={current.img} alt={current.name}/>
