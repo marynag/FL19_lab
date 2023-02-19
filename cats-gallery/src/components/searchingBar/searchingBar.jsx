54bf9e1f0cd9370c553c4c51dfb31df3525a8c62
marynag <mgrabchak1109@gmail.com>
2023-02-19 21:31:25 +0200

    fix searching breed by breedName due to breedNameOd changing

--- cats-gallery/src/components/searchingBar/searchingBar.jsx
+++ cats-gallery/src/components/searchingBar/searchingBar.jsx
@@ -1,4 +1,4 @@
-import SearchPanel from '../../img/Vector.png'
+import searchPanel from '../../img/Vector.png'
 import styles from './searchingBar.module.scss'
 import React from 'react';
 import {REACTIONS} from "./searchingBar.constants";
@@ -12,19 +12,20 @@ export const SearchingBar = (props) => {
     const breedNameId = getBreedNameId(breedInfo)
 
     const handleClick = () =>{
-        const breedName=Object.keys(breedNameId).find((key) => {
-             return breedNameId[key].toLowerCase().includes(input.current.value.toLowerCase())
-        })
+        const matchedBreed = Object.entries(breedNameId).find((breed) => {
+            const {name} = breed[1]
+            return (name.toLowerCase().includes(input.current.value.toLowerCase()) )
+         })
 
-        const breedId = breedNameId[breedName]
-        props.onChange(breedId)
+        const matchedBreedId = matchedBreed[1].id
+        props.onChange(matchedBreedId)
     }
 
     return(
         <div className={styles.searchingLine}>
                 <div className={styles.search}>
                     <input type="text"  column placeholder="Search for breeds by name" className={styles.searchInput} ref={input}/>
-                    <div className={styles.searchItem} onClick={handleClick}><img src={SearchPanel} alt="search" className={styles.search_img}/></div>
+                    <div className={styles.searchItem} onClick={handleClick}><img src={searchPanel} alt="search" className={styles.search_img}/></div>
                 </div>
             { REACTIONS.map(current => (<div key={current.name} className={styles.reaction}>
                 <img src={current.img} alt={current.name}/>
