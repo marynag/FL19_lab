b7e0298f52be9a62c4c86d122c310bcca9ff6919
marynag <mgrabchak1109@gmail.com>
2023-02-19 21:39:17 +0200

    replace reducer to map for converting arr to arr

--- cats-gallery/src/components/breedsPhotoLoader/breedsPhotoLoader.utils.js
+++ cats-gallery/src/components/breedsPhotoLoader/breedsPhotoLoader.utils.js
@@ -1,7 +1,6 @@
 export const getUtls = (res) =>{
-    const result= res.reduce((acc, item) =>{
-        acc.push(item.url)
-        return acc
-    },[])
+    const result= res.map((item) =>{
+        return  item.url
+    })
     return result
 }
