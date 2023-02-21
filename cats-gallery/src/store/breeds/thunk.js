232f1a37b9afb1fb062e54dc94a0c95bc7409b4d
marynag <mgrabchak1109@gmail.com>
2023-02-21 22:21:08 +0200

    fix url's name

--- cats-gallery/src/store/breeds/thunk.js
+++ cats-gallery/src/store/breeds/thunk.js
@@ -1,9 +1,9 @@
-import {URL_BREEDS_INFO} from "../../components/requests/requests.constants";
+import {URL_BREEDS} from "../../components/requests/requests.constants";
 import {BREEDS_FETCHED} from "./actionTypes";
 
 export const getBreeds = () =>{
     return (dispatch) =>{
-        fetch(URL_BREEDS_INFO)
+        fetch(URL_BREEDS)
             .then(response => response.json())
             .then(response =>dispatch({ type: BREEDS_FETCHED, payload: response })
             )
