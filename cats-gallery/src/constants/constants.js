3626ef6dfc5ddec87c03616463defc7cf331ac69
marynag <mgrabchak1109@gmail.com>
2023-03-16 20:47:32 +0200

    input and select styles for focus and active

--- cats-gallery/src/constants/constants.js
+++ cats-gallery/src/constants/constants.js
@@ -1,4 +1,5 @@
 export const LIMITS = [5, 10, 15, 20];
 export const TYPE = ['gif', 'jpg,png'];
-export const ORDER = ['RANDOM', 'DESC', 'ASC'];
+export const TYPE_NAME = ['Animated', 'Static'];
+export const ORDER = ['Random', 'Desc', 'Asc'];
 export const IMAGE_SIZE = 'small';
